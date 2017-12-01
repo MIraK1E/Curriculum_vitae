ac61c2d4662736d13d2c05021427032f0cdb05a5
MIraK1E <mike.pattana@gmail.com>
2017-12-01 22:47:51 +0700

    change fadeOut overlay effect

--- source/js/cv_js.js
+++ source/js/cv_js.js
@@ -1,7 +1,7 @@
 function showPage() {
     var typed = new Typed('#loader_text', {strings:["Now's loading..."],typeSpeed: 100});
     setTimeout(function(){
-        $("#overlay").addClass("animated fadeOutUp");
+        $("#overlay").addClass("animated fadeOut");
         $("#page_control").css({'display':'block'});
     },3000);
 }
