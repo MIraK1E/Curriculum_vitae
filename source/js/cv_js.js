e4a32db7448dbef1fea76c61d8c4394545431c13
MIraK1E <mike.pattana@gmail.com>
2017-12-01 22:50:50 +0700

    change overlay effect

--- source/js/cv_js.js
+++ source/js/cv_js.js
@@ -1,7 +1,7 @@
 function showPage() {
     var typed = new Typed('#loader_text', {strings:["Now's loading..."],typeSpeed: 100});
     setTimeout(function(){
-        $("#overlay").addClass("animated fadeOut");
+        $("#overlay").addClass("animated fadeOutDown");
         $("#page_control").css({'display':'block'});
     },3000);
 }
