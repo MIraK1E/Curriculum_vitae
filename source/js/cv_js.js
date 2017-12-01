caf1ec18a69817fef2f5da2fccb6e382e72b625d
MIraK1E <mike.pattana@gmail.com>
2017-12-02 02:09:37 +0700

    fix now loading word

--- source/js/cv_js.js
+++ source/js/cv_js.js
@@ -1,5 +1,5 @@
 function showPage() {
-    var typed = new Typed('#loader_text', {strings:["Now's loading..."],typeSpeed: 100});
+    var typed = new Typed('#loader_text', {strings:["Now loading..."],typeSpeed: 100});
     setTimeout(function(){
         $("#overlay").addClass("animated fadeOutDown");
         $("#page_control").css({'display':'block'});
